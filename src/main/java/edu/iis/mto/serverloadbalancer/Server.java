bb675baf7a45878322988374e60bf27f6f0696e7
Karol Kucharski <187746@edu.p.lodz.pl>
2016-06-15 23:44:53 +0200

    test 2

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,8 +3,14 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
 
 	public double currentLoadPecentage;
+	private int capacity;
 
 	public Server(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public void addVm(Vm vm) {
+		currentLoadPecentage += 100.0 * vm.getSize() / capacity;
 	}
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -21,6 +21,28 @@ public class ServerLoadBalancerTest {
 
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
+	
+	@Test
+	public void balancingAServerWithOneSlotCapacity_andOneSlotVm_fillsServerWithTheVm() {
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+
+		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(100.0d));
+	}
+
+	private Vm a(VmBuilder builder) {
+		return builder.build();
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
+	}
+
+	private Vm[] aListOfVmsWith(Vm... vms) {
+		return vms;
+	}
 
 	private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
